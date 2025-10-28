ee40830c0a34b0294363d61f36cbb710953822d0
Ulrich Renner <ulrich.renner@zf.com>
2025-10-28 12:11:08 +0100

    Add support for entity type App

--- sovd-server/src/apis/discovery.rs
+++ sovd-server/src/apis/discovery.rs
@@ -7,8 +7,9 @@ use sovd_api::{
         ComponentsComponentIdRelatedAppsGetResponse, ComponentsComponentIdSubcomponentsGetResponse,
         EntityCollectionEntityIdGetResponse, EntityCollectionGetResponse,
     },
-    models::{self, AnyPathDocsGetDefaultResponse},
+    models::{self, AnyPathDocsGetDefaultResponse, EntityCollectionEntityIdGet200Response},
 };
+use sovd_handlers::find_single_process;
 
 use crate::ServerImpl;
 
@@ -76,32 +77,83 @@ impl sovd_api::apis::discovery::Discovery for ServerImpl {
     ) -> Result<EntityCollectionEntityIdGetResponse, ()> {
         // Check, if requested entity_id is supported. TODO: include registered
         // sub SOVD Servers. Currently, we support only the component which hosts the SOVD server
-        if path_params.entity_id != self.id {
-            return Ok(
-                EntityCollectionEntityIdGetResponse::Status0_AnUnexpectedRequestOccurred(
-                    AnyPathDocsGetDefaultResponse::new(
-                        "not-responding".to_owned(),
-                        format!("Component {} did not respond.", path_params.entity_id),
-                    ),
-                ),
-            );
+        match (path_params.entity_collection.as_str()) {
+            "areas" => todo!(),
+            "components" => {
+                if path_params.entity_id != self.id {
+                    return Ok(
+                        EntityCollectionEntityIdGetResponse::Status0_AnUnexpectedRequestOccurred(
+                            AnyPathDocsGetDefaultResponse::new(
+                                "not-responding".to_owned(),
+                                format!("Component {} did not respond.", path_params.entity_id),
+                            ),
+                        ),
+                    );
+                }
+
+                let mut response = models::EntityCollectionEntityIdGet200Response::new(
+                    self.id.to_owned(),
+                    self.name.to_owned(),
+                );
+
+                let _ = response.data.insert(format!(
+                    "http://{}{}/components/{}/data",
+                    host.0,
+                    sovd_api::BASE_PATH,
+                    self.id
+                ));
+
+                Ok(EntityCollectionEntityIdGetResponse::Status200_TheResponseBodyContainsAPropertyForEachSupportedResourceAndRelatedCollection(
+                    response
+                ))
+            },
+            "apps" => {
+                let tokens = path_params.entity_id.split('-');
+
+                // Check, if last token is a number (is the PID in that case)
+                let last_token = tokens.clone().last().unwrap();
+                let pid = match last_token.parse::<u32>() {
+                    Ok(pid) => pid.to_string(),
+                    Err(_) => "".to_string(),
+                };
+
+                let mut resource = String::new();
+                for token in tokens {
+                    if token.ne(last_token) {
+                        resource.push_str(token);
+                        resource.push('-');
+                    } else if pid.is_empty() {
+                        resource.push_str(token);
+                    } else {
+                        resource.remove(resource.len() - 1);
+                    }
+                }
+
+                if let Some(app) = find_single_process(&resource, &pid, &format!("http://{}{}",
+                    host.0,
+                    sovd_api::BASE_PATH)) {
+                    let mut response = EntityCollectionEntityIdGet200Response::new(
+                        path_params.entity_id.clone(),
+                        app.name.clone(),
+                    );
+                    let app_data = format!(
+                        "http://{}{}/{}/{}/data",
+                        host.0,
+                        sovd_api::BASE_PATH,
+                        path_params.entity_collection.to_owned(),
+                        app.id.clone()
+                    );
+
+                    response.data = Some(app_data);
+
+                    return Ok(EntityCollectionEntityIdGetResponse::Status200_TheResponseBodyContainsAPropertyForEachSupportedResourceAndRelatedCollection(response));
+                } else {
+                    todo!()
+                }
+            }
+            "functions" => todo!(),
+            _ => todo!()
         }
-
-        let mut response = models::EntityCollectionEntityIdGet200Response::new(
-            self.id.to_owned(),
-            self.name.to_owned(),
-        );
-
-        let _ = response.data.insert(format!(
-            "http://{}{}/components/{}/data",
-            host.0,
-            sovd_api::BASE_PATH,
-            self.id
-        ));
-
-        Ok(EntityCollectionEntityIdGetResponse::Status200_TheResponseBodyContainsAPropertyForEachSupportedResourceAndRelatedCollection(
-        response
-      ))
     }
 
     /// EntityCollectionGet - GET /v1/{entity_collection}
@@ -114,23 +166,34 @@ impl sovd_api::apis::discovery::Discovery for ServerImpl {
         path_params: &models::EntityCollectionGetPathParams,
         query_params: &models::EntityCollectionGetQueryParams,
     ) -> Result<EntityCollectionGetResponse, ()> {
-        let mut items = Vec::<models::EntityCollectionGet200ResponseItemsInner>::new();
-
         // TODO: Besides self, sub SOVD Servers registering via mDNS must be
         // supported here.
-        items.push(models::EntityCollectionGet200ResponseItemsInner::new(
-            self.id.to_owned(),
-            self.name.to_owned(),
-            String::from(format!(
-                "http://{}{}/components/{}",
-                host.0,
-                sovd_api::BASE_PATH,
-                self.id
-            )),
-        ));
+        match path_params.entity_collection.as_str() {
+            "areas" => todo!(),
+            "components" => {
+                let mut items = Vec::<models::EntityCollectionGet200ResponseItemsInner>::new();
+                items.push(models::EntityCollectionGet200ResponseItemsInner::new(
+                    self.id.to_owned(),
+                    self.name.to_owned(),
+                    String::from(format!(
+                        "http://{}{}/components/{}",
+                        host.0,
+                        sovd_api::BASE_PATH,
+                        self.id
+                    )),
+                ));
 
-        Ok(EntityCollectionGetResponse::Status200_ResponseBody(
-            models::EntityCollectionGet200Response::new(items),
-        ))
+                Ok(EntityCollectionGetResponse::Status200_ResponseBody(
+                    models::EntityCollectionGet200Response::new(items),
+                ))
+            },
+            "apps" => {
+                Ok(EntityCollectionGetResponse::Status0_AnUnexpectedRequestOccurred(
+                    models::AnyPathDocsGetDefaultResponse::new("-1".to_owned(), "Not implemented yet".to_owned())
+                ))
+            },
+            "functions" => todo!(),
+            _ => todo!()
+        }
     }
 }
